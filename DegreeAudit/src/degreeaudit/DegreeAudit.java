8aac3049babd4d00d43320a7220dad2cdaff8805
whud99 <whud99@gmail.com>
2017-10-25 21:46:15 -0400

    William
    William's Branch

--- DegreeAudit/src/degreeaudit/DegreeAudit.java
+++ DegreeAudit/src/degreeaudit/DegreeAudit.java
@@ -4,18 +4,33 @@
  * and open the template in the editor.
  */
 package degreeaudit;
+import java.util.ArrayList;
 
-/**
- *
- * @author cmarzari
- */
+//class template to hold the curriculum information 
+class ClassInfo { 
+    String Name;
+    int CorseId;
+    int[] pre;  // pre = prerequisites
+    int hours;
+    
+    public ClassInfo(String Names,int Ids, int[] pres,int credits) {
+        //copy vars to the main object
+        Name = Names;
+        CorseId = Ids;
+        pre = pres;
+        hours = credits;
+   }
+}
+   
 public class DegreeAudit {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Huge List O' Curriculum
+        //ClassInfo NAME = new ClassInfo("NAME", 0000, new int[]{} , 0);
+        ClassInfo CSET1100 = new ClassInfo("Intro to CSET", 1100,new int[]{1100}, 4);
+        ClassInfo EET2420  = new ClassInfo("Instrumentation Laboratory", 2420,new int[]{} , 1);
+        ClassInfo PHIL1010 = new ClassInfo("Introduction to Logic", 1010, new int[]{}, 3);
+        ClassInfo ENGT1000 = new ClassInfo("Introduction to Engineering Technology", 1000, new int[]{} , 1);
+        ClassInfo ENGL1110 = new ClassInfo("NAME", 0000, new int[]{} , 0);
     }
     
 }
