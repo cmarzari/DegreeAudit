39d11a61b6a92b1b626049b5a10011fd1fa3a074
smarzz <marzaric@gmail.com>
2017-10-30 00:16:17 -0400

    playing around with hashmaps'

--- DegreeAudit/src/degreeaudit/DegreeAudit.java
+++ DegreeAudit/src/degreeaudit/DegreeAudit.java
@@ -1,10 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package degreeaudit;
 
+
+package degreeaudit;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Map;
+import java.util.HashMap;
 /**
  *
  * @author cmarzari
@@ -15,7 +15,28 @@ public class DegreeAudit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+    	Scanner input = new Scanner(System.in);
+    	
+    	System.out.println("Input all of the classes you've taken, separated by a comma:");
+    	
+    	//try with a few classes 
+    	
+    	HashMap allCourses = new HashMap();
+    	
+    	allCourses.put("CSET1100", new int 0);
+    	allCourses.put("CSET1200", new int 1);
+    	allCourses.put("CSET1300", new int 2);
+    	
+    	
+    	while(i.hasNext()) {
+    		
+    		Map.Entry course = (Map.Entry)i.next();
+    		System.out.println(course.getKey);
+    	}
+    	
+    	
+    	
     }
     
 }
